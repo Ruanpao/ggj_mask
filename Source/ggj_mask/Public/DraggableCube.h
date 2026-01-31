1f749111cae25375c51b9e4e3df3d9102b2b3c62
yeamyacout <3154448388@qq.com>
2026-01-31 11:02:39 +0800

    添加滑轨

--- Source/ggj_mask/Public/DraggableCube.h
+++ Source/ggj_mask/Public/DraggableCube.h
@@ -21,11 +21,17 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	void StartDragging(AActor* Dragger);
-
 	void StopDragging();
-
 	void UpdateDragging(const FVector& TargetPosition);
 
+	//设置轨道
+	UFUNCTION(BlueprintCallable,Category = "Track")
+	void SetTrackPoints(const TArray<FVector>& Points);
+
+	//自动链接轨道
+	UFUNCTION(BlueprintCallable,Category = "Track")
+	void ConnectToNearestTrack();
+	
 	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category = "Components")
 	class UStaticMeshComponent* CubeMesh;
 
@@ -34,7 +40,32 @@ public:
 
 	UPROPERTY(BlueprintReadOnly,Category = "Dragging")
 	bool bIsBeingDragged = false;
+
+	//轨道点
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "Track")
+	TArray<FVector> TrackPoints;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "Track")
+	bool bUseTrack = true;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "Track")
+	float TrackRadius = 50.f;
 private:
 	FVector DragStartPosition;
 	float DragStartHeight;
+
+	//将位置投影到最近的轨道上
+	FVector ProjectToTrack(const FVector& Position);
+	
+	//找到轨道上最近的点
+	FVector FindNearestPointOnTrack(const FVector& Position);
+	
+	//找到最近两个轨道点直接的线段
+	FVector FindNearestPointOnSegment(const FVector& Position);
+
+	//找到最近的轨道线段索引
+	int32 FindNearestSegmentIndex(const FVector& Position);
+	
+	//当前轨道线段索引
+	int32 CurrentTrackSegmentIndex = -1;
 };
