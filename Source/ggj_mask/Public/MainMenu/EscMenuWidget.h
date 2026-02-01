fbd89c9cf93dbbad6b768440003aa80246c0749f
Ruanpao <ltcdz114514@gmail.com>
2026-02-01 08:58:12 +0800

    escMenu

---
+++ Source/ggj_mask/Public/MainMenu/EscMenuWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MainMenu/MainMenuWidget.h"
+#include "EscMenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGJ_MASK_API UEscMenuWidget : public UMainMenuWidget
+{
+	GENERATED_BODY()
+	
+};
